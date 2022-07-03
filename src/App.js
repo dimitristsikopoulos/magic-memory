ff5fbe3d86a82b2f43f6ebf5a30d1fc00a9c5814
Dimitris Tsikopoulos <dimitristsik@gmail.com>
2022-07-03 15:45:40 +0300

    set project up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const cardImages = [{ src: "/img/helmet-1.png" }, { src: "/img/potion-1.png" }];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
